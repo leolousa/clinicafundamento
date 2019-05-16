d655ebd1ccc1cbf8577b152edfe2809bee23b750
leolousa <leolousa@hotmail.com>
2019-05-16 11:37:55 -0300

    Evoluindo o Layout

---
+++ js/scrollprogress.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  progressbar = document.querySelector('.progress-bar');
+  progressbar.style.setProperty('--progresso', scrolled);
+}
+
