751e592446136ef6a218687982aa9b7e93f21c79
leolousa <leolousa@hotmail.com>
2019-05-20 11:36:40 -0300

    Evoluindo o layout com Google Maps

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -4,13 +4,30 @@ function initMap() {
   var fundamento = {lat: -15.831523, lng: -48.017480};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 18, center: fundamento});
+      document.getElementById('map'), {zoom: 17, center: fundamento});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({
     position: fundamento,
     map: map,
     title: 'Clínica FundamenTO',
-    icon:'img/fundamento@64.png',
-    animation: google.maps.Animation.BOUNCE
+    icon:'img/fundamento@32.png',
+    animation: google.maps.Animation.BOUNCE,
+    disableDefaultUI: true
   });
+  var styles = {
+    default: null,
+    hide: [
+      {
+        featureType: 'poi.business',
+        stylers: [{visibility: 'off'}]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'labels.icon',
+        stylers: [{visibility: 'on'}]
+      }
+    ]
+  };
+
+  map.setOptions({styles: styles['hide']});
 }
