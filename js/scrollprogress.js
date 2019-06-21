9f8de3cc059dbb21e88b8f2d430aac2401a255aa
leolousa <leolousa@hotmail.com>
2019-06-21 11:14:12 -0300

    Versão alterada com Grid Layout

--- js/scrollprogress.js
+++ js/scrollprogress.js
@@ -1,16 +1,16 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() { myFunction() };
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
+
   progressbar = document.querySelector('.cabecalho__progress-bar');
   btnInicio = document.querySelector('.btn-inicio');
-
   progressbar.style.setProperty('--progresso', scrolled);
+
   if (scroll = null || scrolled < 5) {
     btnInicio.style.setProperty('display', 'none');
-
   } else {
     btnInicio.style.setProperty('display', 'block');
   }
